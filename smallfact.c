e52dcf9ae90475e13eff7ea0741d4219dea577d5
piyushbhutani1999 <piyushbhutaniynr@gmail.com>
2018-11-20 02:43:30 -0800

    codechef small fact100

---
+++ smallfact.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{	int num,b[200],a[200],carry=0,s[200],k=0,i,m=1,f;
+	for(i=1;i<200;i++){
+		a[i]=0;
+	}
+	a[0]=1;
+	printf("Entere the number");
+	scanf("%d",&num);
+	i=1;
+	while(i<=num){
+		b[k]=(a[k]*i)+carry;
+		a[k]=b[k]%10;
+		carry=b[k]/10;
+		s[m]=k;
+		if(b[k]==0){
+			k++;
+		}
+		else{
+			if(carry==0){
+				if(m>0&&(k<s[m-1])){
+					k++;
+				}
+			else{
+				i++;
+				m++;
+				f=k;
+				k=0;			
+			}
+			}
+			if(carry!=0){
+				k++;
+			}
+		}
+	}
+	for(i=0;i<=f;i++){
+		printf("%d",a[f-i]);
+	}
+}
